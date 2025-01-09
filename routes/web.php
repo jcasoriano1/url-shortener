dbb12c0aa49306ba40e75e797a03f0ac3b46f6b2
JC <josecarlosoriano@gmail.com>
2025-01-10 02:48:09 +0800

    Register the redirect route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlShortenerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
-require __DIR__.'/auth.php';
+Route::get('/s/{code}', [UrlShortenerController::class, 'redirect']);
