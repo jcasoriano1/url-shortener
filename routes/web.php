5b4e6c8fdf5e4c1282c53bc8b3991bbfde8431bb
JC <josecarlosoriano@gmail.com>
2025-01-10 02:52:58 +0800

    Bring back auth routes as we could use it in the future

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\UrlShortenerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/s/{code}', [UrlShortenerController::class, 'redirect']);
+
+require __DIR__.'/auth.php';
